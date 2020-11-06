72dbb8ca487f5bc8e0fe258b805b24bb778aa771
Alex Johnson <james.vinton.johnson@gmail.com>
2020-11-06 14:51:35 -0500

    Beginning to update room generation for new system

---
+++ Assets/Scripts/EnterDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterDoor : MonoBehaviour
+{
+
+    [SerializeField]
+    string direction;
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            GameObject dungeon = GameObject.FindGameObjectWithTag("Dungeon");
+            DungeonGeneration dungeonGeneration = dungeon.GetComponent<DungeonGeneration> ();
+            Room room = dungeonGeneration.CurrentRoom();
+            dungeonGeneration.MoveToRoom(room.Neighbor(this.direction));
+            SceneManager.LoadScene("RoomGenerator");
+        }
+
+    }
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    //Idea from here https://scholarslab.lib.virginia.edu/blog/teleporting-in-Unity3D/
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+    public Camera previousCam;
+    public Camera nextCam;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        thePlayer.transform.position = teleportTarget.transform.position;
+        nextCam.enabled = true;
+        previousCam.enabled = false;
+    }
+}
