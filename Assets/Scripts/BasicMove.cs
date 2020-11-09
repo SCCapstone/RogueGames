4977d256adabef1c0c1629d1e71c68a8672757ac
Alex Johnson <james.vinton.johnson@gmail.com>
2020-11-09 16:50:12 -0500

    Added "Basic move" script which uses rigidBody2d

---
+++ Assets/Scripts/BasicMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMove: MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody2D rb;
+
+    private float speed = 0.7f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0.0f);
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+
+        rb.velocity = new Vector2(movement.x * speed, movement.y * speed);
+
+    }
+}
