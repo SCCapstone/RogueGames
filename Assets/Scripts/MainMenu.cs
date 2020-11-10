589d194f4d527d631f956fe9383434b0da84094e
James Thompson <james.thompson2500@gmail.com>
2020-11-09 19:32:57 -0500

    Menu/UI prototype rebuilt - Nov 9

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Function for the Play button. Starts the prototype.
+    public void PlayGame() {
+        Debug.Log("Starting game!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    // Function for the Quit button. Quits the game.
+    public void QuitGame() {
+    	Debug.Log("Quitting game from Menu!");
+    	Application.Quit();
+    }
+}
