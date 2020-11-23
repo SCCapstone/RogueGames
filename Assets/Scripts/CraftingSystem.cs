3bc8c68f9f33906db78d54dfafc98856a33e0b52
lukasm-usc <69805332+lukasm-usc@users.noreply.github.com>
2020-11-23 18:07:03 -0500

    Update CraftingSystem.cs

--- Assets/Scripts/CraftingSystem.cs
+++ Assets/Scripts/CraftingSystem.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CraftingSystem : IItemHolder {
+public class CraftingSystem : ItemInterface{
 
     public const int GRID_SIZE = 2;
 
