378e99f45d3b6461558e3f361494b932a4249d87
lukasm-usc <69805332+lukasm-usc@users.noreply.github.com>
2021-02-28 10:52:16 -0500

    Delete Assets directory

--- Assets/Scripts/Combat/Weapon.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public abstract class Weapon : MonoBehaviour {
-  public float baseDegrade;
-  public float durability = 1f;
-
-  public abstract void Attack(Vector3 attackDir);
-}
