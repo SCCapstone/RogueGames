3d82d13cf0fed19d9e6bcc8dc3f3c0f0601755fc
James Thompson <69982882+jameset2599@users.noreply.github.com>
2021-03-29 16:04:05 -0400

    Added "Welcome"/controls message to game.
    When a run is started/restarted, a screen appears before the gameplay begins to welcome the player and show the controls. (Not all controls are listed, may need to be discussed later.)

---
+++ Assets/Scripts/Menu and UI/FirstTimeControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTimeControls : MonoBehaviour
+{
+    public GameObject gameControls;
+    public GameObject health;
+    public GameObject inventory;
+
+    void Start()
+    {
+        Time.timeScale = 0f;
+    }
+
+    void Update()
+    {
+        if(Input.anyKey)
+        {
+            gameControls.SetActive(false);
+            health.SetActive(true);
+            inventory.SetActive(true);
+            Time.timeScale = 1f;
+        }
+    }
+}
